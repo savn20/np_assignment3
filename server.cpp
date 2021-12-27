755c12d73bb4e86a6577a9b80e2a9d2053e12f05
savn20 <savn20@student.bth.se>
2021-12-28 00:23:04 +0530

    chore: server select setup

--- server.cpp
+++ server.cpp
@@ -8,12 +8,20 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/time.h>
 
 // comment the DEBUG macro to turn off comments in the console
 #define DEBUG
 #define BACKLOG 5
+#define USER_LIMIT 50
 
 using namespace std;
+
+struct chatUser
+{
+    char nickname[20];
+    int socket;
+};
 
 int main(int argc, char *argv[])
 {
@@ -42,16 +50,28 @@ int main(int argc, char *argv[])
 
     int serverSocket = -1,
         clientSocket = -1,
+        maxSocket = -1,
+        interrupt = -1,
         acceptMultipleClients = 1;
 
     /*************************************/
     /*   setting up server metadata     */
     /***********************************/
-    sockaddr_in serverAddress;
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(serverPort);
+    address.sin_addr.s_addr = inet_addr(serverIp.c_str());
 
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(serverPort);
-    serverAddress.sin_addr.s_addr = inet_addr(serverIp.c_str());
+    fd_set socketSet;           // stores multiple sockets
+    chatUser users[USER_LIMIT]; //users who wants to chat
+
+    char nickname[15];
+    char message[256];
+    string connectProtocol = "HELLO 1\n";
+
+    // initialise users
+    for (int i = 0; i < USER_LIMIT; i++)
+        users[i].socket = 0;
 
     /*************************************/
     /*   creating socket connection     */
@@ -79,7 +99,7 @@ int main(int argc, char *argv[])
     /*************************************/
     /*  binding server to ip and port   */
     /***********************************/
-    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    if (bind(serverSocket, (struct sockaddr *)&address, sizeof(address)) < 0)
     {
         close(serverSocket);
         cerr << "error: failed to bind socket\n"
@@ -99,6 +119,81 @@ int main(int argc, char *argv[])
     }
 
     printf("server started listening on %s:%d\n", serverIp.c_str(), serverPort);
+    int addrLen = sizeof(address);
+
+    while (1)
+    {
+
+        /**********************************************/
+        /*   setup to monitor all the active client  */
+        /********************************************/
+        FD_ZERO(&socketSet);
+
+        FD_SET(serverSocket, &socketSet);
+        maxSocket = serverSocket;
+
+        for (int i = 0; i < USER_LIMIT; i++)
+        {
+            clientSocket = users[i].socket;
+
+            if (clientSocket > 0)
+                FD_SET(clientSocket, &socketSet);
+
+            if (clientSocket > maxSocket)
+                maxSocket = clientSocket;
+        }
+
+        /***************************************************/
+        /*  listen for interrupts from server and client  */
+        /*************************************************/
+        interrupt = select(maxSocket + 1, &socketSet, NULL, NULL, NULL);
+
+        if ((interrupt < 0) && (errno != EINTR))
+            cerr << "error: failed to listen interrupts" << endl;
+
+        // new client connected
+        if (FD_ISSET(serverSocket, &socketSet))
+        {
+            if ((clientSocket = accept(serverSocket,
+					(struct sockaddr *)&address, (socklen_t*)&addrLen))<0)
+            {
+                cerr << "error: can't accept client\n"
+                     << "program terminated while accept()" << endl;
+                close(serverSocket);
+                exit(-1);
+            }
+
+            //inform user of socket number - used in send and receive commands
+            printf("client connected from %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+
+            /*************************************/
+            /*  connect() protocol -> HELLO 1   */
+            /***********************************/
+            if (send(clientSocket, connectProtocol.c_str(), strlen(connectProtocol.c_str()), 0) < 0)
+            {
+                cerr << "error: failed to send message to client\n"
+                     << "program terminated while send()" << endl;
+                close(clientSocket);
+                exit(-1);
+            }
+
+            printf("server protocol: HELLO 1\n");
+
+            //add new socket to array of sockets
+            for (int i = 0; i < USER_LIMIT; i++)
+            {
+
+                if (users[i].socket == 0)
+                {
+                    users[i].socket = clientSocket;
+                    cout << "Adding client to list at " << i + 1 << endl;
+                    break;
+                }
+            }
+        }
+
+        //TODO: read, check and accept nickname
+    }
 
     return 0;
 }
